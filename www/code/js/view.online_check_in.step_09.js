8c68e6c249cefdf33ab7e9d9c0c2a7be5ed59acf
Greg Shin <gshin@causeforce.com>
2017-04-17 13:15:56 -0700

    initial commit
    Signed-off-by: Greg Shin <gshin@causeforce.com>

---
+++ www/code/js/view.online_check_in.step_09.js
@@ -0,0 +1,5 @@
+/************************************************************************
+ * @author	Kingsley U. Uwode II, contractor via Robert Half Technologies
+ * @since	June 1 2015
+ * @link	mailto:kingzmeadow@hotmail.com
+ */
